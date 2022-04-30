f3fcb278a4af2ae9fd999d256205002373f19f0a
Ibnul Ashir <ibnulashir42@gmail.com>
2022-04-30 13:29:30 +0600

    layout

---
+++ src/layout/Content/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Index() {
+  return (
+    <section>
+      <p>sjdbvjbSDJB</p>
+      <p>sjdbvjbSDJB</p>
+    </section>
+  );
+}
+
+export default Index;
---
+++ src/layout/Footer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Index() {
+  return <footer>Footer</footer>;
+}
+
+export default Index;
